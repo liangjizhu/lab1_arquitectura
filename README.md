f2a04f2ecb5047ba8d9ab2e16300e0c6d8073fa2
Alfonso Pineda <agpfven@gmail.com>
2024-10-06 17:50:36 +0200

    Meter ejemplos, modificar CMakeLists y hacer un ejemplo en imtool-aos

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # lab1_arquitectura
+## Para usar
+ cmake -S . -B debug
+
+## "Make clean"
+rm -r debug/
--- imtool-aos/main.cpp
+++ imtool-aos/main.cpp
@@ -1,3 +1,9 @@
 //
 // Created by liang on 4/10/24.
 //
+#include <iostream>
+
+int main(){
+      std::cout << "test" << "\n";
+      return 0;
+}
