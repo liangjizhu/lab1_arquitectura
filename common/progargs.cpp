aea6ec8adab68854fc9447453774c1a0c421b31b
Liang <100495723@alumnos.uc3m.es>
2024-11-10 18:54:08 +0100

    progargsTests

--- common/progargs.cpp
+++ common/progargs.cpp
@@ -6,6 +6,7 @@
 
 constexpr uint16_t MAX_COLOR_VALUE_16BIT = 65535;
 constexpr size_t RESIZE_ARGUMENT_COUNT = 5;
+constexpr size_t MAX_LEVEL_ARGUMENT_COUNT = 5;
 
 // Constructor para inicializar los argumentos usando std::vector<std::string>
 ProgramArgs::ProgramArgs(const std::vector<std::string>& arguments) : args(arguments) {}
@@ -59,24 +60,26 @@ bool ProgramArgs::validateInfo() const{
     return true;
 }
 
-bool ProgramArgs::validateMaxLevel() const{
-    if (args.size() != 4) {
-        errorMessage = "Error: Invalid number of extra arguments for maxlevel";
+bool ProgramArgs::validateMaxLevel() const {
+    if (args.size() != MAX_LEVEL_ARGUMENT_COUNT) {
+        errorMessage = "Error: Invalid number of arguments for 'maxlevel'. Expected: 5 (imtool input output maxlevel xxx)";
         return false;
     }
     try {
-        int const maxLevel = std::stoi(args[3]);
+        int const maxLevel = getMaxLevel();
         if (maxLevel < 0 || maxLevel > MAX_COLOR_VALUE_16BIT) {
-            errorMessage = "Error: Invalid maxlevel: " + args[3];
+            errorMessage = "Error: Invalid maxlevel value: " + std::to_string(maxLevel) + ". It must be between 0 and " + std::to_string(MAX_COLOR_VALUE_16BIT);
             return false;
         }
     } catch (const std::invalid_argument&) {
-        errorMessage = "Error: Invalid maxlevel: " + args[3];
+        errorMessage = "Error: Invalid maxlevel value. It must be an integer.";
+        return false;
+    } catch (const std::out_of_range&) {
+        errorMessage = "Error: Maxlevel value is out of range.";
         return false;
     }
     return true;
 }
-
 
 bool ProgramArgs::validateResize() const{
     if (args.size() != RESIZE_ARGUMENT_COUNT) {
@@ -148,7 +151,7 @@ std::string ProgramArgs::getErrorMessage() const {
 
 int ProgramArgs::getMaxLevel() const {
     try {
-        return std::stoi(args[3]);
+        return std::stoi(args[4]);
     } catch (const std::exception& e) {
         std::cerr << "Error: Invalid max level value: " << e.what() << '\n';
         return -1;
--- utest-common/binaryio_test.cpp
+++ utest-common/binaryio_test.cpp
@@ -1,3 +1,12 @@
-//
-// Created by liang on 8/10/24.
-//
+#include "common/binaryio.hpp"
+#include <gtest/gtest.h>
+#include <vector>
+
+TEST(BinaryIOTest, ReadBinaryFile_ValidFile) {
+    std::vector<uint8_t> data = BinaryIO::readBinaryFile("../input/deer-small.ppm");
+    ASSERT_FALSE(data.empty());
+}
+
+TEST(BinaryIOTest, ReadBinaryFile_InvalidFile) {
+    ASSERT_THROW(BinaryIO::readBinaryFile("nonexistent.ppm"), std::runtime_error);
+}
